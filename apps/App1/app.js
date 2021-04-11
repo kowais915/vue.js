88eef7b3140785ab6cedb1a700e000f342a016c6
kowais915 <kowais915@gmail.com>
2021-04-11 06:00:17 +0500

    learning vue!

---
+++ apps/App1/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+//    template: '<h2>I am the template.</h2>'
+
+data()
+{
+   return {
+       title: 'Mohammad Owais Khan',
+       major: 'Computer Engineering',
+       school: 'Ghulam Ishaq Khan Institute of engineering sciences & Technology'
+
+   }
+}
+
+})
+
+app.mount('#app')
