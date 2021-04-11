a030e446b5c507e3b65f83268e0b31d66911d33b
kowais915 <kowais915@gmail.com>
2021-04-11 06:45:16 +0500

    created a smol app

--- apps/App1/app.js
+++ apps/App1/app.js
@@ -9,6 +9,33 @@ data()
        school: 'Ghulam Ishaq Khan Institute of engineering sciences & Technology',
        age: 20
    }
+},
+
+methods: {
+   change(){
+
+    // // pick random student
+    //     student = ['Owais', 'Ammad', 'Saad', 'Haris']
+
+    //     for(i = 0; i<student.length; i++)
+    //     {
+    //         this.title = student[i];
+    //     }
+
+    var name = window.prompt("Enter the name of the studetn: ")
+    var majoruser =window.prompt("Enter the major of the student: ")
+    var ageuser = window.prompt("Enter the age of the student: ")
+
+
+
+    this.title = name
+    this.major = majoruser
+    this.age = ageuser
+      
+   }
+
+
+
 }
 
 })
